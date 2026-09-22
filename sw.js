082d552ad88795a847f915f5302662775e07b0d9
Kaushalji <harshitkaushal9129@gmail.com>
2026-09-22 21:29:06 +0530

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'upi-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
